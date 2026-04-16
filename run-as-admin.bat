66d69296f38658aa0dbb746fe09051a038ce198b
Mayank <may.pan.rt24@dypatil.edu>
2026-04-16 21:20:22 +0530

    feat: add administrator privilege detection and run-as-admin.bat script

---
+++ run-as-admin.bat
@@ -0,0 +1,25 @@
+@echo off
+:: This script checks for Admin privileges and requests them if missing.
+:: It then starts the TrustWipe Backend.
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Requesting Administrator privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%~dp0"
+    echo ------------------------------------------------
+    echo  TRUSTWIPE: RUNNING WITH ADMIN PRIVILEGES
+    echo ------------------------------------------------
+    cd trustwipe-backend
+    .\mvnw.cmd spring-boot:run
